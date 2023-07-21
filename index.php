04152f6db6edbd113fd37f543f0f3ba241a3c5c8
Savitcha <savitcha.p@gmail.com>
2023-07-21 08:19:41 +0700

    test21/7/23

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+
+<body>
+
+    <h1> ศูนบริการ </h1>
+
+    <h2> Welcome </h2>
+    <p> กรุณาเลือกการทำรายการ </p>
+
+    <a href="pagedata1.html">ลอง</a>
+    <button onclick=" ">ยื่นคำขอรับบริการ</button>
+    <button onclick=" ">ตรวจสถานะ</button>
+
+    PHP file
+
+    <?php echo "TES" ?>
+
+
+</body>
+
+</html>
